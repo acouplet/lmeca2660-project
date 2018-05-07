e5a5d1c36864a3b13998d9459f46a03add4af245
Adrien Couplet <coupleta@oasis.uclouvain.be>
2018-05-07 15:09:34 +0200

    Code review with MP

--- code/main.c
+++ code/main.c
@@ -17,8 +17,8 @@ const double g = 9.81;
 int main(){	
 	clock_t begin = clock();
 
-    int Nx       = 200;
-    int Ny       = 300;
+    int Nx       = 4;
+    int Ny       = 1.5*Nx;
     double h     = 1.0/Ny;
     double Pr    = 2.0;
     double dt    = 0.1;
@@ -27,6 +27,7 @@ int main(){
     double Tinf  = -5e-3;
     double Gr    = 2.0e10;
     double alpha = 1.97;
+	double H;
 
     double sumR      = 0.0;
     double globe     = 0.0;
@@ -41,10 +42,10 @@ int main(){
     double **v       = matrix(Ny+1,Nx+2);
     double **ustar   = matrix(Ny+2,Nx+1);
     double **vstar   = matrix(Ny+1,Nx+2);
-    double **Hnpx    = matrix(Ny,Nx);
-    double **Hnpy    = matrix(Ny,Nx);
-    double **Phi     = matrix(Ny+2,Nx+2);
-    double **Phistar = matrix(Ny+2,Nx+2);
+    double **Hnpx    = matrix(Ny,Nx); // Change to Nx-1
+    double **Hnpy    = matrix(Ny,Nx); // Change to Ny-1
+    double **Phi     = matrix(Ny+2,Nx+2); // Change to (Ny,Nx)
+    double **Phistar = matrix(Ny+2,Nx+2); // Change to (Ny,Nx)
     double **R       = matrix(Ny,Nx);
     double **HnpT    = matrix(Ny,Nx);
 
@@ -52,36 +53,37 @@ int main(){
 	FILE *fT = fopen("data/T_400_600.csv","w");
 	FILE *fu = fopen("data/u_400_600.csv","w");
 	FILE *fv = fopen("data/v_400_600.csv","w");
-    //F(i,Ny) F(j,Nx) P[i][j] = g*(Ny-i-0.5)*h; // Probably wrong, any idea how to make initial P dimensionless
-
+    
     F(k,10000){
     //================//
     // First equation //
     //================//
-    F(i,Ny+2) { u[i][0] = 0; u[i][Nx] = 0; } // No-slip condition 
-    F(j,Nx+2) { v[0][j] = 0; v[Ny][j] = 0; }
-    F(j,Nx+1) { u[0][j] = u[1][j]; u[Ny+1][j] = u[Ny][j]; } // Ghost points, no-through flow
-    F(i,Ny+1) { v[i][0]    = -0.2*(v[i][3]    - 5*v[i][2]    + 15*v[i][1]);
-                v[i][Nx+1] = -0.2*(v[i][Nx-2] - 5*v[i][Nx-1] + 15*v[i][Nx]); }
+	// No-slip conditions not necessary, never modified
+    //F(i,Ny+2) { u[i][0] = 0; u[i][Nx] = 0; } // No-slip condition 
+    //F(j,Nx+2) { v[0][j] = 0; v[Ny][j] = 0; }
+    //F(j,Nx+1) { u[0][j] = u[1][j]; // Ghost points, no-through flow
+	//			u[Ny+1][j] = -0.2*(u[Ny-2][j] - 5*u[Ny-1][j] + 15*u[Ny][j]); } // CORRECTED Ghost point (prev: du/dy = 0, now: u_gamma = 0)
+    //F(i,Ny+1) { v[i][0]    = -0.2*(v[i][3]    - 5*v[i][2]    + 15*v[i][1]);
+    //            v[i][Nx+1] = -0.2*(v[i][Nx-2] - 5*v[i][Nx-1] + 15*v[i][Nx]); }
 
     FR(i,1,Ny+1){
         FR(j,1,Nx){
             ustar[i][j]    = -1*NS_pressurex(i,j,P,h);
             ustar[i][j]   += (1/sqrt(Gr))*NS_diffusionx(i,j,u,h);
-            double H       = NS_convectionx(i,j,u,v,h);
+            H       	   = NS_convectionx(i,j,u,v,h);
             ustar[i][j]   -= 0.5*(3*H - Hnpx[i-1][j-1]);
             Hnpx[i-1][j-1] = H;
             ustar[i][j]    = dt*ustar[i][j] + u[i][j];
         }
     }
-    F(j,Nx+1) { ustar[0][j] = ustar[1][j]; ustar[Ny+1][j] = ustar[Ny][j]; } // Ghost points
+    F(j,Nx+1) { ustar[0][j] = ustar[1][j]; ustar[Ny+1][j] = ustar[Ny][j]; } // WRONG, SEE CORRECTED ABOVE
 
     FR(i,1,Ny){
         FR(j,1,Nx+1){
             vstar[i][j]    = -1*NS_pressurey(i,j,P,h);
             vstar[i][j]   += (1/sqrt(Gr))*NS_diffusiony(i,j,v,h);
             vstar[i][j]   -= NS_buoyancy(i,j,T);
-            double H       = NS_convectiony(i,j,u,v,h);
+            H       	   = NS_convectiony(i,j,u,v,h);
             vstar[i][j]   -= 0.5*(3*H - Hnpy[i-1][j-1]);
             Hnpy[i-1][j-1] = H;
             vstar[i][j]    = dt*vstar[i][j] + v[i][j];
@@ -102,7 +104,7 @@ int main(){
     Trms = 0.0;
     FR(i,1,Ny+1){
         FR(j,1,Nx+1){
-            double H       = (T[i][j+1]-T[i][j-1])*(u[i][j]+u[i][j-1])/(2*h) + (T[i-1][j] - T[i+1][j])*(v[i-1][j]+v[i][j])/(2*h);
+            H       	   = (T[i][j+1]-T[i][j-1])*(u[i][j]+u[i][j-1])/(2*h) + (T[i-1][j] - T[i+1][j])*(v[i-1][j]+v[i][j])/(2*h);
             double Tn1     = -0.5*(3*H - HnpT[i-1][j-1]);
             HnpT[i-1][j-1] = H;
             Tn1           += (1/(Pr*sqrt(Gr)*h*h))*((T[i][j+1]-2*T[i][j]+T[i][j-1])+(T[i-1][j]-2*T[i][j]+T[i+1][j]));
@@ -129,6 +131,7 @@ int main(){
     while(globe > 1e-3){
         FR(i,1,Ny+1){
             FR(j,1,Nx+1){
+				// Ghost points for PHI ? 
                 Phistar[i][j] = (h*h)/4*(-1/dt*((ustar[i][j]-ustar[i][j-1])/h + (vstar[i-1][j]-vstar[i][j])/h) + (Phi[i+1][j] + Phi[i-1][j])/(h*h) + (Phi[i][j+1] + Phi[i][j-1])/(h*h));
                 Phi[i][j] = alpha*Phistar[i][j] + (1-alpha)*Phi[i][j];
             }
@@ -136,12 +139,13 @@ int main(){
         sumR = 0;
         F(i,Ny){
             F(j,Nx){
+				// VERIFY INDICES
                 R[i][j] = (Phi[i+2][j+1]-2*Phi[i+1][j+1]+Phi[i][j+1])/(h*h) + (Phi[i+1][j+2]-2*Phi[i+1][j+1]+Phi[i+1][j])/(h*h) - 1/dt*((ustar[i+1][j+1]-ustar[i+1][j])/h + (vstar[i][j+1]-vstar[i+1][j+1])/h);
                 sumR += R[i][j]*R[i][j];
             }
         }
         globe = dt*sqrt((3*h*h)/2*sumR);
-        iter = iter+1;
+        iter += 1;
         //printf("Sum local residual = %lf\n",sumR);
         //printf("Global error = %lf\n",globe);
     }
@@ -159,9 +163,11 @@ int main(){
         FR(j,1,Nx+1)
             v[i][j] = -(dt/h)*(Phi[i][j]-Phi[i+1][j]) + vstar[i][j];
 
-    F(i,Ny+2) { u[i][0] = 0; u[i][Nx] = 0; } // No-slip condition 
-    F(j,Nx+2) { v[0][j] = 0; v[Ny][j] = 0; }
-    F(j,Nx+1) { u[0][j] = u[1][j]; u[Ny+1][j] = u[Ny][j]; } // Ghost points, no-through flow
+	// No-slip conditions not necessary, never modified
+    //F(i,Ny+2) { u[i][0] = 0; u[i][Nx] = 0; } // No-slip condition 
+    //F(j,Nx+2) { v[0][j] = 0; v[Ny][j] = 0; }
+    F(j,Nx+1) { u[0][j] = u[1][j]; // Ghost points, no-through flow
+				u[Ny+1][j] = -0.2*(u[Ny-2][j] - 5*u[Ny-1][j] + 15*u[Ny][j]); } // CORRECTED Ghost point (prev: du/dy = 0, now: u_gamma = 0)
     F(i,Ny+1) { v[i][0]    = -0.2*(v[i][3]    - 5*v[i][2]    + 15*v[i][1]);
                 v[i][Nx+1] = -0.2*(v[i][Nx-2] - 5*v[i][Nx-1] + 15*v[i][Nx]); }
 
@@ -170,13 +176,14 @@ int main(){
     //============//
     F(i,Ny)
         F(j,Nx)
-            P[i][j] = P[i][j] + Phi[i+1][j+1];
+            P[i][j] += Phi[i+1][j+1];
     
 
     printf("SOR Iterations = %d\n",iter);
     printf("Tavg = %.4lf\n",Tavg);
     printf("Trms = %.4lf\n",Trms);
 
+	
     FR(i,0,Ny+2){
         FR(j,0,Nx+2)
             printf("%.4lf\t",T[i][j]);
